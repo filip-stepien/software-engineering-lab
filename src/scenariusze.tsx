8b84af9cfade7992d29374ccd93fc05e17340252
rafal <you@example.com>
2024-11-06 18:11:23 +0100

    use case table init

---
+++ src/scenariusze.tsx
@@ -0,0 +1,27 @@
+import { Usecase } from './usecase.tsx';
+
+const scenarios: Array<Usecase> = [
+    {
+        name: 'Nauka tabliczki mnożenia',
+        actors: ['pracownik', 'klient'],
+        usecase: 'Janas',
+        preconditions: ['60lvl w życie', 'dr inż'],
+        postconditions: ['poprawka zdana'],
+        mainFlow: [
+            {
+                num: 1,
+                text: 'Pomnóż 3 niewiadomo ile razy',
+            },
+            {
+                num: 2,
+                text: 'Nie zdaj wyjściówki',
+            },
+        ],
+        alternateFlow: [
+            {
+                num: 5,
+                text: '1 Miej kalkulator GOTO EXIT',
+            },
+        ],
+    },
+];
---
+++ src/usecase.tsx
@@ -0,0 +1,23 @@
+type Actor = 'pracownik' | 'kierownik' | 'klient' | 'trener';
+
+interface Author {
+    name: string;
+    surname: string;
+    group: string;
+}
+
+interface FlowItem {
+    num: number;
+    text: string;
+}
+
+export interface Usecase {
+    name: string;
+    preconditions: Array<string>;
+    postconditions: Array<string>;
+    actors: Array<Actor>;
+    mainFlow: Array<FlowItem>;
+    alternateFlow?: Array<FlowItem>;
+    usecase: string;
+    author?: Author;
+}
